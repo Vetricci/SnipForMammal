9c58dfb8c158e56bae3e59df3696ee65687e3d77
Vetricci <81270549+Vetricci@users.noreply.github.com>
2021-07-14 21:34:31 -0500

    Added code to disallow multiple instances from running concurrently.

--- Program.cs
+++ Program.cs
@@ -11,24 +11,36 @@ namespace SnipForMammal
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            // Run only if no other instance is already running.
+            //System.Diagnostics.Process[] processSnipForMammalInstances = System.Diagnostics.Process.GetProcessesByName("SnipForMammal");
 
-            // Globals
-            Global.outputFormat = Properties.Settings.Default.outputFormat;
-            Global.updateAuthTokenInterval = Properties.Settings.Default.updateAuthTokenInterval;
-            Global.updateSpotifyTrackInfoInterval = Properties.Settings.Default.updateSpotifyTrackInfoInterval;
-            Global.updateCurrentTrackPlayingInterval = Properties.Settings.Default.updateCurrentTrackPlayingInterval;
-            Global.autoCloseAuthWindow = Properties.Settings.Default.autoCloseAuthWindow;
-            Global.browser = Properties.Settings.Default.browser;
-            Global.debugConsole = new DebugConsole();
-            Global.settings = new Settings();
-            Global.customTextEntryForm = new CustomTextEntryForm();
-            Global.spotify = new Spotify();
-            Global.snipForMammal = new SnipForMammal(); // Keep this line out of Application.Run() to ensure the form never shows and instantly minimizes.
+            if (System.Diagnostics.Process.GetProcessesByName("SnipForMammal").Length <= 1)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
 
-            // Run
-            Application.Run();
+                // Globals
+                Global.outputFormat = Properties.Settings.Default.outputFormat;
+                Global.updateAuthTokenInterval = Properties.Settings.Default.updateAuthTokenInterval;
+                Global.updateSpotifyTrackInfoInterval = Properties.Settings.Default.updateSpotifyTrackInfoInterval;
+                Global.updateCurrentTrackPlayingInterval = Properties.Settings.Default.updateCurrentTrackPlayingInterval;
+                Global.autoCloseAuthWindow = Properties.Settings.Default.autoCloseAuthWindow;
+                Global.browser = Properties.Settings.Default.browser;
+                Global.debugConsole = new DebugConsole();
+                Global.settings = new Settings();
+                Global.customTextEntryForm = new CustomTextEntryForm();
+                Global.spotify = new Spotify();
+                Global.snipForMammal = new SnipForMammal(); // Keep this line out of Application.Run() to ensure the form never shows and instantly minimizes.
+
+                // Run
+                Application.Run();
+            }
+            else
+            {
+                MessageBox.Show("Error: There is already an instance of SnipForMammal.exe running.");
+                Application.Exit();
+            }
+
 
 
         }
